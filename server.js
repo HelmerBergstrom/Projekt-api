bfd7b31e63b21ff5cf090fa58a11a50946e64dc8
Helmer Bergström <hebe2400@student.miun.se>
2025-06-12 18:00:12 +0200

    Skapar mapp där scheman ska läggas. Kopplar till databasen i mongoDB.

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3003;
 
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/giffers").then(() => {
+    console.log("Connected to MongoDB :)")
+}).catch((error) => {
+    console.log("Error connecting to database: " + error); 
+});
+
+const routes = require("./routes");
+app.use("/api", routes)
 
 
 app.listen(port, () => {
