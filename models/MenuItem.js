6e4a19ae03882c68e1b3c0766258407bd3a0939c
Helmer Bergström <hebe2400@student.miun.se>
2025-06-12 18:10:23 +0200

    Schema för menyobjekt.

---
+++ models/MenuItem.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const MenuItemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ["Förrätt", "Varmrätt", "Efterrätt", "Dryck"]
+    },
+});
+
+module.exports = mongoose.model("MenuItem", MenuItemSchema);
