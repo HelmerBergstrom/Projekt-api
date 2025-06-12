f8be47fc178e92b9a8ea43567b334cd173b1c071
Helmer Bergström <hebe2400@student.miun.se>
2025-06-12 22:25:44 +0200

    Skapar ny model för User, som är till admin-delen. Skapar även fil där hemliga routes ska läggas.

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Krypterar lösenordet innan det sparas i databasen.
+UserSchema.pre("save", async function(next) {
+    try {
+        if(this.isNew || this.isModified("password")) {
+            const hashedPass = await bcrypt.hash(this.password, 10)
+            this.password = hashedPass;
+        }
+        next();
+    } catch(error) {
+        next(error)
+    };
+});
+
+// Metod för att jämföra lösenord vid inloggning till Admin-sidan.
+UserSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
