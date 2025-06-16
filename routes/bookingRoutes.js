ce42530328e5cb8bdc42927bbb2c66cdd017958f
Helmer Bergström <hebe2400@student.miun.se>
2025-06-16 19:53:44 +0200

    Validering för att boka bord. Datum måste vara framåt i tiden.

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -17,11 +17,19 @@ router.post("/", async (req, res) => {
     try {
         const { fullName, phone, date, time, guests } = req.body;
 
+        const bookingDate = new Date(date);
+        const todaysDate = new Date();
+        todaysDate.setHours(0, 0, 0, 0); // För att sätta tiden i date-objektet till 00:00:00.
+
         if(!fullName || !phone || !date || !time || !guests ) {
             return res.status(400).json({ message: "Namn, telefonnummer, datum, tid och antal gäster måste fyllas i!"});
         }
         if(guests > 6) {
             return res.status(400).json({ message: "Antal gäster måste vara under sju!"});
+        }
+
+        if(bookingDate < todaysDate) {
+            return res.status(400).json({ message: "Du kan endast boka framåt i tiden eller för senare samma dag!" });
         }
 
         const booking = new Booking(req.body);
